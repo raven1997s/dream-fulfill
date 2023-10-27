d34d5d27a7ee45064d83ffddc8cc7659c9df62ae
raven <gaoning176@gmail.com>
2023-10-27 14:41:56 +0800

    fix sth

--- src/main/java/com/raven/dreamfulfill/DreamFulfillApplication.java
+++ src/main/java/com/raven/dreamfulfill/DreamFulfillApplication.java
@@ -12,7 +12,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class DreamFulfillApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
-        //
         SpringApplication.run(DreamFulfillApplication.class, args);
     }
     @Bean
