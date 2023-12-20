db825e26d72412f3f553395d82fcd8bd09574b28
raven <52941626+raven1997s@users.noreply.github.com>
2023-12-20 19:33:56 +0800

    Update DreamFulfillApplication.java

--- src/main/java/com/raven/dreamfulfill/DreamFulfillApplication.java
+++ src/main/java/com/raven/dreamfulfill/DreamFulfillApplication.java
@@ -20,7 +20,7 @@ public class DreamFulfillApplication implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8090") // 允许特定源
+                        .allowedOrigins("http://localhost:8090", "http://www.igaon.cn") // 允许特定源
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允许的 HTTP 方法
                         .allowCredentials(true) // 允许发送身份凭证
                         .allowedHeaders("*"); // 允许的请求头
